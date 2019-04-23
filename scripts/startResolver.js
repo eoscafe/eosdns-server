b595f88691d40820f028ff7d7e3fd671237f41a1
Syed Jafri <syed.jafri2@ucalgary.ca>
2019-04-23 08:26:57 -0600

    test custom resolver

--- scripts/startResolver.js
+++ scripts/startResolver.js
@@ -1,5 +1,5 @@
 const kill = require('kill-port')
-const RecursiveServer = require('../src/server')
+const RecursiveServer = require('../src/customResolver')
 const NODE_URL = process.env.NODE_URL
 const enable = require('./enable');
 
