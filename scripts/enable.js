aae040c812dbe910b983d593b7d4c14eaa6c51a2
Syed Jafri <syed.jafri2@ucalgary.ca>
2019-04-23 03:38:38 -0600

    update edit-dns

--- scripts/enable.js
+++ scripts/enable.js
@@ -5,7 +5,10 @@ const NS = '127.0.0.1'
 
 async function main () {
   // Saves current DNS settings
+  console.log(11)
+  console.log(dnsEditor)
   await dnsEditor.save()
+  console.log(12)
 
   // Load new DNS settings
   await dnsEditor.load([NS])
