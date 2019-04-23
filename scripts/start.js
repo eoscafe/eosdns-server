49891e6febe7c121e60c67e25fe09aff1a9da0e1
Syed Jafri <syed.jafri2@ucalgary.ca>
2019-04-23 03:39:12 -0600

    remove consoles

--- scripts/start.js
+++ scripts/start.js
@@ -5,10 +5,7 @@ const enable = require('./enable');
 
 (async () => {
   await kill(53)
-  console.log(1);
-
   await enable()
-  console.log(2);
 
   const recursiveServer = new RecursiveServer({
     port: 53,
