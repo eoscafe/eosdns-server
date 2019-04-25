e032f83610310f8af9df0df438b03e7db97a7acf
Syed Jafri <syed.jafri2@ucalgary.ca>
2019-04-24 21:45:13 -0600

    test exit hook

--- scripts/start.js
+++ scripts/start.js
@@ -2,9 +2,17 @@ const kill = require('kill-port')
 const RecursiveServer = require('../src/server')
 const NODE_URL = process.env.NODE_URL
 const DnsEditor = require('edit-dns')
+const exitHook = require('../src/asyncExit');
 const dnsEditor = new DnsEditor('EOSDNS')
 
 const NS = '127.0.0.1'
+
+async function exit (err) {
+  console.log(err)
+  await dnsEditor.recover()
+}
+exitHook.uncaughtExceptionHandler(async err => await exit(err));
+exitHook.unhandledRejectionHandler(async err => await exit(err));
 
 async function main () {
   await kill(53)
